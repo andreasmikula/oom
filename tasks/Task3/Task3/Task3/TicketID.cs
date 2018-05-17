581c752e887b6215c05f318db68db8b60010fa30
Andreas Mikula <ic17b026@technikum-wien.at>
2018-05-17 10:56:52 +0200

    add project Task3

---
+++ tasks/Task3/Task3/Task3/TicketID.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class TicketID : IAttend
+    {
+        private int Id;
+        public decimal Price { get; set; }
+
+        public TicketID(int id, decimal price)
+        {
+            Id = id;
+            Price = price;
+        }
+
+        public void updatePrice(int price)
+        {
+            if (price <= Price)
+                Price = price;
+            throw new ArgumentOutOfRangeException("Your price " + price.ToString() + "is smaller than current price " + Price.ToString());
+        }
+
+        public bool Presence()
+        {
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}, {1}, {2}", Id.ToString(), Price.ToString(), Presence().ToString());
+        }
+    }
+}
