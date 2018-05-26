a14e975b107ea723d176e7fd41ef5054844dc515
Andreas Mikula <ic17b026@technikum-wien.at>
2018-05-26 10:25:11 +0200

    add 8 default tests, test no.5 fails.

---
+++ tasks/Task4/Task4/Task4/Concert.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Concert : IAttend
+    {
+        private string Band;
+        public string Date { get; }
+        public string Location { get; set; }
+        public int Price { get; set; }
+
+        public Concert(string band, string date, string location, int price)
+        {
+            Band = band;
+            Date = date;
+            Location = location;
+            Price = price;
+        }
+
+        public void updatePrice(int price)
+        {
+            if (price >= Price)
+                Price = price;
+            else
+                throw new ArgumentOutOfRangeException("Your price " + price.ToString() + " is smaller than current price " + Price.ToString());
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}, {1}, {2}, {3}, {4}\n", Band, Date, Location, Price.ToString(), Presence());
+        }
+        public bool Presence()
+        {
+            return true;
+        }
+    }
+}
---
+++ tasks/Task4/Task4/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IAttend[] attends = new IAttend[]
+                {
+            new Concert("Godsmack", "17.11.2018", "Arena Wien", 34),
+            new TicketID(10, 37.90M),
+                };
+
+            foreach (IAttend x in attends)
+            {
+                Console.WriteLine(x.ToString());
+            }
+        }
+    }
+}
