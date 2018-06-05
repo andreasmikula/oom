6b189e65eaacb5c35a4ef1f5b6107a1236c28bfd
Andreas Mikula <ic17b026@technikum-wien.at>
2018-06-05 19:04:41 +0200

    add Task6 on base Task4 incl. install of System.Reactive package

---
+++ tasks/Task6/Task6/Task6/Program.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IAttend[] attends = new IAttend[]
+                {
+            new Concert("Godsmack", "17.11.2018", "Arena Wien", 34),
+            new Concert("Lamb of God", "11.11.2018", "Stadthalle Wien", 54),
+            new TicketID(10, 37.90M),
+            new TicketID(11, 55.90M),
+                };
+
+            JsonSerializerSettings settings = new JsonSerializerSettings();
+            settings.TypeNameHandling = TypeNameHandling.Auto;
+            settings.Formatting = Formatting.Indented;
+
+            using (System.IO.StreamWriter streamWriter = new System.IO.StreamWriter(System.IO.File.Open("jsonFile.json", System.IO.FileMode.Create)))
+            {
+
+                streamWriter.Write(JsonConvert.SerializeObject(attends, settings));
+                streamWriter.Flush();
+
+            }
+
+            string json = System.IO.File.ReadAllText("jsonFile.json");
+
+            IAttend[] readCreatures = JsonConvert.DeserializeObject<IAttend[]>(json, settings);
+
+            Console.WriteLine(json);
+
+        }
+    }
+}
---
+++ tasks/Task6/Task6/Task6/Tests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Task6
+{
+    [TestFixture]
+
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            TicketID ticketid = new TicketID(4, 39M);
+            Assert.IsFalse(ticketid.Presence());
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Concert concert = new Concert("Test", "Test", "Test", 70);
+            Assert.IsTrue(concert.Presence());
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Concert concert = new Concert("Test", "Test", "Test", 180);
+            Assert.IsTrue(concert.Price == 180);
+            concert.updatePrice(190);
+            Assert.IsTrue(concert.Price == 190);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            Concert concert = new Concert("Test", "Test", "Test", 180);
+            var except = Assert.Throws<ArgumentOutOfRangeException>(() => concert.updatePrice(100));
+            Assert.That(except.ParamName, Is.EqualTo("Your price 100 is smaller than current price 180"));
+        }
+
+        [Test]
+        public void Test5()
+        {
+            TicketID ticketid = new TicketID(4, 14M);
+            Assert.IsTrue(ticketid.getID() == 4);
+            ticketid.updatePrice(15);
+            Assert.IsFalse(ticketid.getID() == 15);
+        }
+
+        [Test]
+        public void Test6()
+        {
+            TicketID ticketid = new TicketID(4, 39M);
+            var except = Assert.Throws<ArgumentOutOfRangeException>(() => ticketid.updatePrice(9));
+            Assert.That(except.ParamName, Is.EqualTo("Your price 9 is smaller than current price 39"));
+        }
+
+        [Test]
+        public void Test7()
+        {
+            Concert concert = new Concert("Test", "Test", "Test", 70);
+            Assert.That(concert.ToString(), Is.EqualTo("Test, Test, Test, 70, True\n"));
+        }
+
+        [Test]
+        public void Test8()
+        {
+            TicketID ticketid = new TicketID(10, 0.1M);
+            Assert.That(ticketid.ToString(), Is.EqualTo("10, 0,1, False"));
+        }
+    }
+}
